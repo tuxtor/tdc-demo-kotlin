bc81d22cf29f0b1d8021fe12a4b0b9f1cf922acb
Víctor Orozco <vorozco@nabenik.com>
2021-06-10 00:38:02 -0600

    Adding CLI capabilities with picocli

--- src/main/kotlin/com/nabenik/tdc/App.kt
+++ src/main/kotlin/com/nabenik/tdc/App.kt
@@ -1,14 +1,31 @@
 package com.nabenik.tdc
 
-import kotlin.jvm.JvmStatic
+import picocli.CommandLine
+import java.io.File
+import java.nio.file.Files
+import java.util.concurrent.Callable
+import kotlin.system.exitProcess
 
 /**
  * Hello world!
  *
  */
-object App {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        println("Hello ${args.getOrNull(0)?:"World"}")
+@CommandLine.Command(name = "App", mixinStandardHelpOptions = true, version = ["app 0.0"],
+    description = ["Prints a list of hello phrases from a given file"])
+class App: Callable<Int> {
+
+    @CommandLine.Parameters(index = "0", description = ["File to iterate over greetings"])
+    lateinit var file: File
+
+    override fun call(): Int {
+        val fileContents = Files.readAllLines(file.toPath())
+        fileContents.forEach { println(it) }
+        return 0
     }
+
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) : Unit = exitProcess(CommandLine(App()).execute(*args))
+    }
+
 }
