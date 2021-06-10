b4cbf884851b673ffbd5bac618dd9d64920241e5
Víctor Orozco <vorozco@nabenik.com>
2021-06-09 23:55:01 -0600

    Base quickstart maven

---
+++ src/main/java/com/nabenik/App.java
@@ -0,0 +1,13 @@
+package com.nabenik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
